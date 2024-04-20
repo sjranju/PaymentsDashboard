3a5222c5cb9a48717bef05ac5131a962f73123d8
sjranju <sjranju@gmail.com>
2024-04-20 23:25:22 +0530

    Backend folder structure change to make it work on Netlify

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -1,20 +1,18 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
-import express from "express";
-import serverless from "serverless-http";
+import express from 'express';
+import serverless from 'serverless-http';
+import { router as rootRouter } from './routes/index.js'
 import Config from './config/index.js';
 import Cors from 'cors';
-import { router as rootRouter } from './routes/index.js'
-const { port } = Config.server;
 
-const api = express();
-// Add CORS headers to allow everything
-app.use(Cors());
-app.use(express.json())
-
-export const handler = serverless(api);
-
-app.use('/api/', rootRouter);
+export async function handler(event, context) {
+    const app = express();
+    const { port } = Config.server;
+    // Add CORS headers to allow everything
+    app.use(Cors());
+    app.use(express.json())
+    app.use('/api/', rootRouter);
+    return serverless(app)(event, context);
+};
 
 // Start it listening.
-app.listen(port, () => console.log(`Payments data app listening on port ${port}!`));
+// app.listen(port, () => console.log(`Payments data app listening on port ${port}!`));
